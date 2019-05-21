49f8d3545def864d927975bfa5091895886afcf8
Siemel Naran <SiemelNaran@yahoo.com>
2019-05-20 21:10:52 -0700

    simple algorithm to computer additions/deletions/changes to one string to make it into another

--- src/main/python/analyzediff.py
+++ src/main/python/analyzediff.py
@@ -2,31 +2,31 @@
 
 import itertools
 
+from operator import attrgetter
+
 
 class Segment:
     """
     Class representing a substring in two strings.
 
     Members:
-    - length (int): The length of the substring, may be zero
+    - length (int): The length of the substring, always 1 or more
     - fIndex (int): The index of the last char in the first string
     - sIndex (int): The index of the last char in the second string
     """
 
     @staticmethod
     def empty_segment_start_of_string():
-        segment = Segment(-1, -1)
-        segment.length = 0
+        segment = Segment(0, -1, -1)
         return segment
 
     @staticmethod
     def empty_segment_end_of_string(first, second):
-        segment = Segment(len(first) - 1, len(second) - 1)
-        segment.length = 0
+        segment = Segment(0, len(first) - 1, len(second) - 1)
         return segment
 
-    def __init__(self, fIndex, sIndex):
-        self.length = 1
+    def __init__(self, length, fIndex, sIndex):
+        self.length = length
         self.fIndex = fIndex
         self.sIndex = sIndex
 
@@ -36,7 +36,7 @@ class Segment:
 
 class Group:
     """
-    Class representing a sequence of segments.
+    Class representing a sequence of non-contiguous segments.
     Rather than storing a list of segments, we store a pointer to the parent group (if any) and the last segment.
 
     Members:
@@ -59,62 +59,79 @@ class Group:
         segments.append(self.segment)
         return segments
 
-    def try_extend_segment(self, fIndex, sIndex):
+    def try_extend(self, fIndex, sIndex):
         top = self.top()
         if top.fIndex + 1 == fIndex and top.sIndex + 1 == sIndex:
-            top.length += 1
-            top.fIndex += 1
-            top.sIndex += 1
-            return True
+            segment = Segment(top.length + 1, top.fIndex + 1, top.sIndex + 1)
+            return Group(self.parent, segment)
         else:
-            return False
+            return None
+
+    def __lt__(self, other):
+        self_lengths = [segment.length for segment in self.get_segments()]
+        other_lengths = [segment.length for segment in other.get_segments()]
+        self_lengths.sort(reverse=True)
+        other_lengths.sort(reverse=True)
+        return self_lengths < other_lengths
 
     def __repr__(self):
         return str(self.get_segments())
 
 
-class LineDiff:
-    def __init__(self, first, second):
-        self.group = LineDiff.compute_group(first, second)
+def linediff(first, second):
+    """
+    Compare two strings and return the similarities as a list of segments.
+    A similarity means two substrings of the same characters.
 
-    @staticmethod
-    def compute_group(first, second):
-        groups = [Group(None, Segment.empty_segment_start_of_string())]
-        for fIndex, fChar in enumerate(first):
-            new_groups = []
-            for group in groups:
-                segment = group.top()
-                sIndex = second.find(fChar, segment.sIndex + 1)
-                if sIndex >= 0:
-                    add_additional_groups = group.length = 0
-                    if group.try_extend_segment(fIndex, sIndex):
-                        new_groups.append(group)
-                    else:
-                        new_groups.append(Group(group, Segment(fIndex, sIndex)))
-                        add_additional_groups = True
-                    if add_additional_groups:
-                        while True:
-                            sIndex = second.find(fChar, sIndex + 1)
-                            if sIndex == -1:
-                                break
-                            new_groups.append(Group(group, Segment(fIndex, sIndex)))
-            if any(new_groups):
-                LineDiff.remove_lowest_groups(new_groups)
-                groups = new_groups
-        return groups[0]
+    Running time is O(N^2) where N is the length of each string
+    - There is an outer loop for over the characters in the string, so N iterations
+    - In the inner loop we call find on the second string, which is O(N), hence O(N^2)
+    - In the inner loop we add up to O(N) groups to an array, running time is now like 2*O(N^2) which is still O(N^2)
+    - We finally sort the groups using quicksort, so running time like 2*O(N^2) +N*lg(N)
 
-    @staticmethod
-    def remove_lowest_groups(groups):
-        maxlength = max(map(lambda group: group.top().length, groups))
-        remove_if(groups, lambda group: group.top().length < maxlength)
+    Return: The best list of segments describing similarities, or the empty list if there are no similarities
+    """
 
-    def __repr__(self):
-        return str(self.group)
+    groups = []
+    for fIndex, fChar in enumerate(first):
+        new_groups = []
+
+        for group in itertools.chain(groups, [None]):
+            sIndex = (second.find(fChar, group.top().sIndex + 1) if group is not None
+                                                                 else second.find(fChar))
+            if sIndex >= 0:
+                new_group = group.try_extend(fIndex, sIndex) if group is not None else None
+                if new_group is None:
+                    new_group = Group(group, Segment(1, fIndex, sIndex))
+                new_groups.append(new_group)
+                while True:
+                    sIndex = second.find(fChar, sIndex + 1)
+                    if sIndex == -1:
+                        break
+                    new_group = Group(group, Segment(1, fIndex, sIndex))
+                    new_groups.append(new_group)
+
+        if any(new_groups):
+            # of the new segments added, only keep those with the longest length (i.e. only keep segments that have been extended)
+            # this optimization saves time and memory, but will not find the longest common substring
+            new_segment_max_length = max(map(lambda group: group.top().length, new_groups))
+            removeif_inplace(lambda group: group.top().length < new_segment_max_length, new_groups)
+
+            # add the new groups
+            groups.extend(new_groups)
+
+            # sort by groups with longest segment first
+            groups.sort(reverse=True)
+
+    if not any(groups):
+        return []
+    else:
+        return groups[0].get_segments()
 
 
 class Replacement:
     """
-    Class representing a replacement of text upon an original string.
+    Class representing a replacement, addition, or removal of text upon an original string.
 
     Members:
     - range (range): A range describing the range of text to replace. A length of zero means we are adding text to the original.
@@ -125,6 +142,9 @@ class Replacement:
         self.range = range
         self.text = text
 
+    def equivalent(self, other):
+        return len(self.range) == len(other.range) and self.text == other.text
+
     def __repr__(self):
         return str(self.range) + " -> " + self.text
 
@@ -132,22 +152,23 @@ class Replacement:
 class Replacements:
     def __init__(self, first, second):
         self.replacements = []
-        diff = LineDiff(first, second)
-        segments = diff.group.get_segments()
-        prev_segment = segments[0]
-        for segment in itertools.chain(itertools.islice(segments, 1, len(segments)),
-                                      [Segment.empty_segment_end_of_string(first, second)]):
-            old_range = range(prev_segment.fIndex + 1, segment.fIndex - segment.length + 1)
-            new_text = second[prev_segment.sIndex + 1 : segment.sIndex - segment.length + 1]
-            if len(old_range) == 0 and len(new_text) == 0:
-                continue
-            self.replacements.append(Replacement(old_range, new_text))
-            prev_segment = segment
+        if len(first) > 0 or len(second) > 0:
+            segments = itertools.chain([Segment.empty_segment_start_of_string()],
+                                       linediff(first, second),
+                                       [Segment.empty_segment_end_of_string(first, second)])
+            prev_segment = next(segments)
+            for segment in segments:
+                old_range = range(prev_segment.fIndex + 1, segment.fIndex - segment.length + 1)
+                new_text = second[prev_segment.sIndex + 1 : segment.sIndex - segment.length + 1]
+                if len(old_range) > 0 or len(new_text) > 0:
+                    self.replacements.append(Replacement(old_range, new_text))
+                prev_segment = segment
 
     def __repr__(self):
         return str(self.replacements)
 
-def remove_if(array, predicate):
+
+def removeif_inplace(predicate, array):
     """
     removes elements from array in place that match the given predicate
     running time O(N)
@@ -155,10 +176,10 @@ def remove_if(array, predicate):
     i = 0
     for i, elem in enumerate(array):
         if predicate(elem):
-            __remove_if_internal(array, predicate, i + 1)
+            __removeif_inplace_internal(predicate, array, i + 1)
     return
 
-def __remove_if_internal(array, predicate, i):
+def __removeif_inplace_internal(predicate, array, i):
     offset = 1
     length = len(array)
     while i < length:
--- src/test/python/analyzediff_test.py
+++ src/test/python/analyzediff_test.py
@@ -1,60 +1,130 @@
 import sys
+import traceback
+
+from itertools import takewhile
 
 sys.path.append("../../main/python")
 from analyzediff import Replacements
 
-def assert_equals(expected, actual):
-    if expected != actual:
-        print("!!! expected " + str(expected) + " but got " + str(actual))
-        print("Exiting")
-        sys.exit(1)
+
+class AssertionFailures:
+    def __init__(self):
+        self.num_failures = 0
+
+    def assert_equals(self, expected, actual):
+        if expected != actual:
+            if self.num_failures == 0:
+                print("\n")
+            self.num_failures += 1
+            print("!!! expected " + str(expected) + " but got " + str(actual))
+            stack = takewhile(lambda elem: elem.name != "assert_equals", traceback.extract_stack(limit=2))
+            print("".join(traceback.format_list(stack)))
+
+    def assert_true(self, actual):
+        if not actual:
+            if self.num_failures == 0:
+                print("\n")
+            self.num_failures += 1
+            print("!!! expected True but got False")
+            stack = takewhile(lambda elem: elem.name != "assert_true", traceback.extract_stack(limit=2))
+            print("".join(traceback.format_list(stack)))
+
+    def throw_if_failures(self):
+        if self.num_failures > 0:
+            raise AssertionError(str(self.num_failures) + " failures")
+
+
         
 
+def test_replacements():
+    asserter = AssertionFailures()
+
+    try:
+        # empty string
+        replacements = Replacements("", "")
+        asserter.assert_equals("[]", str(replacements))
+
+        # strings identical
+        replacements = Replacements("abc", "abc")
+        asserter.assert_equals("[]", str(replacements))
+
+        # add entire string
+        replacements = Replacements("", "abc")
+        asserter.assert_equals("[range(0, 0) -> abc]", str(replacements))
+
+        # delete entire string
+        replacements = Replacements("abc", "")
+        asserter.assert_equals("[range(0, 3) -> ]", str(replacements))
+
+        # replace entire string
+        replacements = Replacements("ab", "cdef")
+        asserter.assert_equals("[range(0, 2) -> cdef]", str(replacements))
+
+        # add text in middle (one addition)
+        replacements = Replacements("xgetone", "xgetnowtrone")
+        asserter.assert_equals("[range(4, 4) -> nowtr]", str(replacements))
+
+        # replace text in middle (one replacement)
+        replacements = Replacements("xgetABone", "xgetnowtrone")
+        asserter.assert_equals("[range(4, 6) -> nowtr]", str(replacements))
+
+        # delete text in middle (one deletion)
+        replacements = Replacements("xgetnowtrone", "xgetone")
+        asserter.assert_equals("[range(4, 9) -> ]", str(replacements))
+
+        # add text at end (one addition)
+        replacements = Replacements("ab", "abcdef")
+        asserter.assert_equals("[range(2, 2) -> cdef]", str(replacements))
+
+        # replace text at end (one replacement)
+        replacements = Replacements("xgetoneAB", "xgetonenowtr")
+        asserter.assert_equals("[range(7, 9) -> nowtr]", str(replacements))
+
+        # delete text at end (one deletion)
+        replacements = Replacements("xgetonenowtr", "xgetone")
+        asserter.assert_equals("[range(7, 12) -> ]", str(replacements))
+
+        # add text at start (one addition)
+        replacements = Replacements("ab", "cdefab")
+        asserter.assert_equals("[range(0, 0) -> cdef]", str(replacements))
+
+        # replace text at start (one replacement)
+        replacements = Replacements("ABxgetone", "nowtrxgetone")
+        asserter.assert_equals("[range(0, 2) -> nowtr]", str(replacements))
+
+        # delete text at start (one deletion)
+        replacements = Replacements("nowtrxgetone", "xgetone")
+        asserter.assert_equals("[range(0, 5) -> ]", str(replacements))
+
+        # longest common substring
+        replacements = Replacements("abcde", "-ab+bcd*")
+        # longest common substring is "bcd" so insert "b+" after 1st char, and replace last char "e" with "*"
+        # in other words: asserter.assert_equals("[range(0, 0) -> -, range(1, 1) -> b+, range(4, 5) -> *]", str(replacements))
+        # but our algorithm does not find the longest common substring 
+        # instead the only strings found are "ab" and "cd"
+        asserter.assert_equals("[range(0, 0) -> -, range(2, 2) -> +b, range(4, 5) -> *]", str(replacements))
+
+    finally:
+        asserter.throw_if_failures()
+
+
+def test_equivalent():
+    asserter = AssertionFailures()
+
+    try:
+        replacements1 = Replacements("int val = getValue();", "int val = getIntegerValue();")
+        replacements2 = Replacements("System.out.println(getValue());", "System.out.println(getIntegerValue());")
+        asserter.assert_equals(1, len(replacements1.replacements))
+        asserter.assert_equals(1, len(replacements2.replacements))
+        asserter.assert_true(replacements1.replacements[0].equivalent(replacements2.replacements[0]))
+
+    finally:
+        asserter.throw_if_failures()
+
+
 def main():
-    # strings identical
-    replacements = Replacements("abc", "abc")
-    assert_equals("[]", str(replacements))
-
-    # replace entire string
-    replacements = Replacements("ab", "cdef")
-    assert_equals("[range(0, 2) -> cdef]", str(replacements))
-
-    # add text in middle (one addition)
-    replacements = Replacements("xgetone", "xgetnowtrone")
-    assert_equals("[range(4, 4) -> nowtr]", str(replacements))
-
-    # replace text in middle (one replacement)
-    replacements = Replacements("xgetABone", "xgetnowtrone")
-    assert_equals("[range(4, 6) -> nowtr]", str(replacements))
-
-    # delete text in middle (one deletion)
-    replacements = Replacements("xgetnowtrone", "xgetone")
-    assert_equals("[range(4, 9) -> ]", str(replacements))
-
-    # add text at end (one addition)
-    replacements = Replacements("ab", "abcdef")
-    assert_equals("[range(2, 2) -> cdef]", str(replacements))
-
-    # replace text at end (one replacement)
-    replacements = Replacements("xgetoneAB", "xgetonenowtr")
-    assert_equals("[range(7, 9) -> nowtr]", str(replacements))
-
-    # delete text at end (one deletion)
-    replacements = Replacements("xgetonenowtr", "xgetone")
-    assert_equals("[range(7, 12) -> ]", str(replacements))
-
-    # add text at start (one addition)
-    replacements = Replacements("cdefab", "ab")
-    assert_equals("[range(0, 0) -> cdef]", str(replacements))
-
-    # replace text at start (one replacement)
-    replacements = Replacements("ABxgetone", "nowtrxgetone")
-    assert_equals("[range(0, 2) -> nowtr]", str(replacements))
-
-    # delete text at start (one deletion)
-    replacements = Replacements("nowtrxgetone", "xgetone")
-    assert_equals("[range(0, 5) -> ]", str(replacements))
-
+    test_replacements()
+    test_equivalent()
 
 if __name__ == "__main__":
   main()
