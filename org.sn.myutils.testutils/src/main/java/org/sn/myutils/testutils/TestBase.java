0245aa967757885526da0288d2bc8673f57dc0c6
Siemel Naran <SiemelNaran@yahoo.com>
2020-10-08 00:00:53 -0700

    move parsetree and pubsub and testutils into its own module

---
+++ org.sn.myutils.testutils/src/main/java/org/sn/myutils/testutils/TestBase.java
@@ -0,0 +1,50 @@
+package org.sn.myutils.testutils;
+
+import java.time.Instant;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+
+/**
+ * Base test class that logs when tests are started and end, and when each test is started and ended.
+ * This makes it easier to study console logs of running all tests.
+ * Eclipse does not show the stdout/stderr for each test separately (but in IntelliJ when you click a test you see the stdout/stderr for that test).
+ * 
+ * <p>There is also a function getStartOfTime.
+ */
+@ExtendWith(LogFailureToConsoleTestWatcher.class)
+public abstract class TestBase {
+    private Instant startOfTime;
+
+    @BeforeAll
+    static void onStartAllTests() {
+        System.out.println("start all tests");
+        System.out.println("--------------------------------------------------------------------------------");
+    }
+    
+    @AfterAll
+    static void printAllTestsFinished() {
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("all tests finished");
+    }
+    
+    @BeforeEach
+    void setStartOfTime(TestInfo testInfo) {
+        startOfTime = Instant.now();
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("test started: " + testInfo.getDisplayName());
+    }
+    
+    @AfterEach
+    void printTestFinished(TestInfo testInfo) {
+        System.out.println("test finished: " + testInfo.getDisplayName());
+    }
+    
+    protected final Instant getStartOfTime() {
+        return startOfTime;
+    }
+}
