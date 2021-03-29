dc5ce267dd49a52e202945c80db40a413582c283
Siemel Naran <SiemelNaran@yahoo.com>
2021-03-28 20:45:23 -0700

    test JsonComparisonAssertionError StringJoiner stuff

---
+++ org.sn.myutils.testutils.json/src/test/java/org/sn/myutils/testutils/json/JaonComparisonAssertionErrorTest.java
@@ -0,0 +1,25 @@
+package org.sn.myutils.testutils.json;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import java.util.List;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+
+public class JaonComparisonAssertionErrorTest {
+    @Test
+    public void test() {
+        var errors = List.of("[recordId=r1].firstName=First: Expected First but got FirstWrong",
+                             "[recordId=r1].middleName=ExtraAttribute: Attribute in actual but not in expected");
+        JsonComparisonAssertionError error = new JsonComparisonAssertionError(errors);
+        assertSame(errors, error.getErrors());
+        String message = error.getMessage();
+        assertThat(message, Matchers.startsWith("Expected and actual JSON differ"));
+        assertThat(message, Matchers.stringContainsInOrder("Expected and actual JSON differ",
+                                                           "[recordId=r1].firstName=First: Expected First but got FirstWrong",
+                                                           "[recordId=r1].middleName=ExtraAttribute: Attribute in actual but not in expected"));
+    }
+}
